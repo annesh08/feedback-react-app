cfe49797e0e8556552bcf5fe5a737bffba829cc4
annesh08 <anneshdodo@gmail.com>
2023-01-24 19:30:50 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = "Big Title";
+  const desc = "The quick brown fox jumps over the lazy dog.";
+  const comments = [
+    { id: 1, text: "Comment number 1" },
+    { id: 2, text: "Comment number 2" },
+    { id: 3, text: "Comment number 3" },
+    { id: 4, text: "Comment number 4" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{desc}</p>
+
+      <div className="Comments">
+        <h3>Comments : {comments.length}</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
