b2399a061b46d2f4e0a726ece8ec4daf636f721d
annesh08 <anneshdodo@gmail.com>
2023-01-26 16:17:24 +0530

    Started creating components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-function App() {
-  const title = "Big Title";
-  const desc = "The quick brown fox jumps over the lazy dog.";
-  const comments = [
-    { id: 1, text: "Comment number 1" },
-    { id: 2, text: "Comment number 2" },
-    { id: 3, text: "Comment number 3" },
-    { id: 4, text: "Comment number 4" },
-  ];
-  return (
-    <div className="main">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{desc}</p>
+import Header from "./components/Header";
 
-      <div className="Comments">
-        <h3>Comments : {comments.length}</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="main"></div>
+    </>
   );
 }
 
