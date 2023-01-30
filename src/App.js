7964eecfc49922cab688606551a56210ef5e7b1c
annesh08 <anneshdodo@gmail.com>
2023-01-30 22:04:25 +0530

    Started learning global state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import FeedbackItem from "./components/FeedbackItem";
 import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
+import { useState } from "react";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,16 +4,10 @@ function FeedbackItem() {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState("This is a feedback item");
 
-  const handleClick = () => {
-    setRating((prev) => {
-      return prev + 1;
-    });
-  };
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display"> {text}</div>
-      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
