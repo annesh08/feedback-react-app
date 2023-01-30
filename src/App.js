36799300ed84a2d17bb0d4927ea5cfbd514860d0
annesh08 <anneshdodo@gmail.com>
2023-01-30 21:49:26 +0530

    Learned State and useState Hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import FeedbackItem from "./components/FeedbackItem";
 import Header from "./components/Header";
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <>
       <Header />
       <div className="main"></div>
+      <FeedbackItem />
     </>
   );
 }
